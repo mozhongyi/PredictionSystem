2e2768a2a2e9b91af81d02e5f9cedacc9a986333
mozhongyi <2649053911@qq.com>
2025-06-12 00:06:00 +0800

    将导入超时时间增加到20秒

--- web/src/utils/service.ts
+++ web/src/utils/service.ts
@@ -174,7 +174,7 @@ function createRequestFunction(service: any) {
 			headers: {
 				'Content-Type': get(config, 'headers.Content-Type', 'application/json'),
 			},
-			timeout: 10000,
+			timeout: 20000,
 			baseURL: getBaseURL(),
 			data: {},
 		};
